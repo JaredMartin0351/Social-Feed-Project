0a75d7c45473f107b0313b07a8c1cfa7bb4f8802
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-06 14:43:53 -0600

    added a logo for later use in assets and added a topbar icon onto the page, box for searching

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,42 @@
 import './topbar.css'
+import { Search, Person, Chat } from "@mui/icons-material"
 
 export default function Topbar() {
     return (
         <div className='topbarContainer'>
-            <div className="topbarLeft"></div>
-            <div className="topbarCenter"></div>
-            <div className="topbarRight"></div>
+            <div className="topbarLeft">
+                <span className="logo">Generic Social Media App</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <Search />
+                    <input placeholder='Search for friend, post or video' className="searchInput" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
